2027b4ff667d381137574e816f3bfde7b1693f47
jaime baños <jaimebanossoriano@gmail.com.com>
2023-11-10 18:02:31 +0100

    [ADD] agenda básica con ficheros

--- Agend/main.c
+++ Agend/main.c
@@ -1,77 +1,230 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-// Ejercicio para practicar malloc, free y realloc 
-
-struct Contact *listaContactos;
-int size = 0;
+#define MAX_CONTACTS 100
 
 struct Contact {
-    char name[100];
-    char phoneNumber[20];
-    char email[100];
+    char name[50];
+    char phone[20];
+    char address[100];
 };
 
+void addContact(FILE *fptr, char *file_name);
+void viewContacts(FILE *fptr, char *file_name);
+void searchContact(FILE *fptr, char *file_name);
+void deleteContact(FILE *fptr, char *file_name);
 
-void menu(){
-    printf("AGENDA DE CONTACTOS\n");
-    printf("--------------------\n");
-    printf("1) Agregar un nuevo contacto\n");
-    printf("2) Listar contactos\n");
-    printf("3) Eliminar contacto\n");
-    printf("--------------------\n");
+int main() {
+    int choice;
+    char file_name[20] = "contactos.txt";
+    FILE *fptr;
 
-}
 
-void addUser(){
-    struct Contact contacto;
+    do {
+        printf("\033[1;32m");
+        printf("\n----------------------------------");
+        printf("\033[1;34m");
+        
+        printf("\nSistema de Gestion de Contactos\n");
+        printf("1. Agregar Contacto\n");
+        printf("2. Ver Lista de Contactos\n");
+        printf("3. Buscar Contacto\n");
+        printf("4. Eliminar Contacto\n");
+        printf("0. Salir\n");
+        printf("Ingrese su eleccion: ");
+        printf("\033[1;32m");
+        printf("\n----------------------------------\n");
+        printf("\033[0m");
 
-    printf("Escriba el nombre: ");
-    scanf("%s",contacto.name);
-    printf("Escribe el numero de telefono: ");
-    scanf("%s",contacto.phoneNumber);
-    printf("Escribe el email: ");
-    scanf("%s",contacto.email);
+        scanf("%d", &choice);
 
-    size = size + 1;
-    struct Contact *newContactList = (struct Contact *)realloc(listaContactos, size * sizeof(int));
+        switch (choice) {
+            case 1:
+                addContact(fptr, file_name);
+                break;
+            case 2:
+                viewContacts(fptr, file_name);
+                break;
+            case 3:
+                searchContact(fptr, file_name);
+                break;
+            case 4:
+                deleteContact(fptr, file_name);
+                break;
+            case 0:
+                printf("Saliendo del programa. ¡Hasta luego!\n");
+                break;
+            default:
+                printf("Opcion no valida. Intentelo de nuevo.\n");
+        }
 
-    if (newContactList != NULL){
-        listaContactos = newContactList;
-        listaContactos[size -1] = contacto;
-        printf("USUARIO AGREGADO CON EXITO");
-    }else{
-        printf("ERROR AL CREAR EL USUARIO");
-    }
-}
-
-int main(){
-    
-    int userSelect;
-    listaContactos = (struct Contact *)malloc(size * sizeof(int));
-
-    menu();
-    scanf("%d", &userSelect);
-
-    switch (userSelect)
-    {
-    case 1:
-        addUser();
-        break;
-    case 2:
-        printf("2");
-        break;
-    case 3:
-        printf("4");
-        break;
-    default:
-        printf("Opción no valida");
-        break;
-    }
-
-    for (item)
-
-    free(listaContactos);
+    } while (choice != 0);
 
     return 0;
+}
+
+void addContact(FILE *fptr, char *file_name) {
+    struct Contact contacto;
+
+    printf("\033[1;34m");
+    printf("Escriba su nombre: \n");
+    printf("\033[0m");
+    scanf("%s", contacto.name);
+
+    printf("\033[1;34m");
+    printf("Escriba su email: \n");
+    printf("\033[0m");
+    scanf("%s", contacto.address);
+
+    printf("\033[1;34m");
+    printf("Escriba su telefono: \n");
+    printf("\033[0m");
+    scanf("%s", contacto.phone);
+
+    fptr = fopen(file_name, "a");
+
+    fprintf(fptr, 
+        "Nombre: %s\nEmail: %s\nTelefono: %s\n*\n",contacto.name, contacto.address, contacto.phone
+    );
+
+    fclose(fptr);
+}
+
+void viewContacts(FILE *fptr, char *file_name) {
+
+    printf("---------Agenda-----------\n");
+    
+    fptr = fopen(file_name, "r");
+    
+    if (fptr != NULL){
+        char context[40];
+        while(fgets(context, 40, fptr)) {
+            if (context[0] != '*'){
+                printf("%s", context);
+            }else{
+                printf("--------------------------\n");
+            }
+            
+        }
+        
+    }else{
+        printf("No hay ningún contacto");
+    }
+
+    fclose(fptr);
+
+}
+
+void searchContact(FILE *fptr, char *file_name) {
+    char email [40];
+    char context [40];
+    int i = 0;
+    struct Contact tempContact;
+    bool isFind = false;
+
+    printf("Escriba el email del usuario: ");
+    scanf("%s", &email);
+
+    fptr = fopen(file_name, "r");
+    while(fgets(context, 40, fptr)) {
+        char *token = strtok(context, " ");
+        token = strtok(NULL, " ");
+        if (context[0] != '*')
+        {   
+            char *token2 = strtok(token, "\n");
+            
+            switch (i)
+            {
+            case 0:
+            
+                strcpy(tempContact.name, token2);
+                break;
+            case 1:
+                strcpy(tempContact.address, token2);
+                break;
+            case 2:
+                strcpy(tempContact.phone, token2);
+                break;
+            default:
+                break;
+            }
+            
+            i++;
+        }else
+
+        {   
+            if (strcmp(tempContact.address, email) == 0) {
+                isFind = true;
+                printf("---------CONTACTO-----------\n");
+                printf("Nombre: %s\n", tempContact.name);
+                printf("Email: %s\n", tempContact.address);
+                printf("Telefono: %s\n", tempContact.phone);
+                printf("--------------------------\n");
+            }
+        }
+          
+    }
+
+    fclose(fptr);
+
+    if (!isFind){
+        printf("\nNo se ha encontrado el contacto");
+    }
+    
+}
+
+void deleteContact(FILE *fptr, char *file_name) {
+    struct Contact tempContact;
+    char context[40];
+    char newText[200] = "";
+    char email[40];
+    int i = 0;
+    
+    printf("Escriba el email del usuario: ");
+    scanf("%s", &email);
+
+    fptr = fopen(file_name, "r");
+    while(fgets(context, 40, fptr)) {
+        char *token = strtok(context, " ");
+        token = strtok(NULL, " ");
+        
+        if (context[0] != '*'){
+            char *token2 = strtok(token, "\n");
+            switch (i)
+            {
+            case 0:
+                strcpy(tempContact.name, token2);
+                break;
+            case 1:
+                strcpy(tempContact.address, token2);
+                break;
+            case 2:
+                strcpy(tempContact.phone, token2);
+                break;
+            default:
+                break;
+            }
+            
+            i++;
+            
+        }else{
+
+            i=0;
+
+            if (strcmp(tempContact.address, email) != 0){
+                snprintf(newText, sizeof(newText), "%sNombre: %s\nEmail: %s\nTelefono: %s\n*\n", newText, tempContact.name, tempContact.address, tempContact.phone);
+            }
+        }
+
+    }
+
+    fclose(fptr);
+
+
+    fptr = fopen(file_name, "w");
+    fprintf(fptr, newText);
+    fclose(fptr);
+    
 }
